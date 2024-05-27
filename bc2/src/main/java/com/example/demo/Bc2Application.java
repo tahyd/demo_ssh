a476f936af75a8e0372e516db389ec1fee8f97c1
tahyd <ta.hyd.003@gmail.com>
2024-05-27 22:00:43 +0530

    initial commit

---
+++ bc2/src/main/java/com/example/demo/Bc2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bc2Application.class, args);
+	}
+
+}
